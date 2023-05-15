523488a9480a665a2f432fa4d131e22609273409
shunichi.horigome@ipmu.jp from Ryzen3990X <shunichi.horigome@ipmu.jp>
2023-05-16 00:27:11 +0900

    torch model implementation (but not worked yet)

--- sampler.py
+++ sampler.py
@@ -1,12 +1,16 @@
+from typing import Any
 import pandas as pd
 import numpy as np
 import emcee 
 import emcee.backends
+import torch
 from astropy.io import ascii
 from astropy.coordinates import SkyCoord,Distance
 import astropy.units as u
 from tqdm import tqdm as tqdm
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
+import itertools
+import os
 
 ############################################
 # # How to use blobs in emcee
@@ -196,32 +200,170 @@ class Sampler:
 
 import swyft
 
-class DSphSimulatior(swyft.Simulator):
+class DSphSimulator(swyft.Simulator):
     """ Simulator class for dSph model.
     """
-    def __init__(self,R_pc,model):
+    def __init__(self,model):
         super().__init__()
         self.transform_samples = swyft.to_numpy32  # convert samples to numpy array
-        self.R_pc = R_pc  # Position of observed stars in pc
         self.model = model  # model class for dSph
+        self.R_pc = self.model.data["R_pc"].values  # Position of observed stars in pc
+        self.dsph_model = self.model.submodels["DSphModel"]  # model class for dSph
+        self.vmem_kms_index = self.model.submodels["FlatPriorModel"].data.index.get_loc("vmem_kms")  # index of "vmem_kms" in model.submodels["PriorModel"].data (pandas.Series)
+        self.store_dir = "DSphSimulator_samples"
+        if not os.path.exists(self.store_dir):
+            os.makedirs(self.store_dir)
+        self.store = {}
+
+    def printlog(self,*txt):
+        prefix = self.__class__.__name__ + ":"
+        print(prefix,*txt)
+
+    def get_store_path(self,fname):
+        return os.path.join(self.store_dir,fname)
+
+    def get_vmem_kms(self,p):
+        """ get vmem_kms from model.
+        """
+        return p[self.vmem_kms_index]
+
 
     def get_slos(self,p):
-            """ get line-of-sight velocity from model.
-            """
-            params = self.model.convert_params(p)
-            self.model.update("all",params)
-            slos = self.model.submodels["DSphModel"].sigmalos_dequad(self.R_pc)
-            return slos
+        """ get line-of-sight velocity from model.
+        """
+        params = self.model.convert_params(p)
+        self.model.update("all",params)
+        slos = self.dsph_model.sigmalos_dequad(self.R_pc)
+        return slos
+
+    
+    def get_vlos_kms(self,vmem_kms,slos):
+        """ get line-of-sight velocity from model.
+        """
+        return vmem_kms + slos * np.random.randn(len(self.R_pc))
+
 
     def build(self,graph):
         """ build the simulator.
         """
-        p = graph.node("p", lambda: self.model.sample())
+        p = graph.node("p", self.model.sample)
         # find "vmem_kms" in model.submodels["PriorModel"].data (pandas.Series)
         # and get the index
-        vmem_kms_index = self.model.submodels["FlatPriorModel"].data.index.get_loc("vmem_kms")
-        vmem_kms = graph.node("vmem_kms", lambda p: p[vmem_kms_index], p)
-
+        vmem_kms = graph.node("vmem_kms", self.get_vmem_kms, p)
         slos = graph.node("slos_kms", self.get_slos, p)  # shape: (nstars,)
-        v_kms = graph.node("vlos_kms", lambda vmem_kms,slos: vmem_kms + slos * np.random.randn(len(self.R_pc)), vmem_kms, slos)
+        v_kms = graph.node("vlos_kms", self.get_vlos_kms, vmem_kms, slos)
+    
+    @property
+    def default_zarr_filename(self):
+        """ default zarr filename.
+        """
+        return f"zarr_{self.model.__class__.__name__}_{self.model.dsph_name}"
+    
+
+    def init_zarrstore(self, N, chunk_size, targets={}, zarr_filename=None):
+        """ initialize zarr store.
+        """
+        zarr_filename = self.default_zarr_filename if zarr_filename is None else zarr_filename
+        self.store[zarr_filename] = swyft.ZarrStore(self.get_store_path(zarr_filename))
+        shapes, dtypes = self.get_shapes_and_dtypes(targets)
+        self.store[zarr_filename].init(N, chunk_size, shapes, dtypes)
+
+    
+    def simulate_in_zarrstore(self,N, chunk_size, batch_size, targets=None, conditions={}, exclude=[], parallel=True, zarr_filename=None):
+        """ simulate in store.
+        """
+        self.init_zarrstore(N, chunk_size, targets, zarr_filename)
+
+        # if (targets is not None) or (conditions is not None):
+        #     _sampler = lambda size: self.sample(size,targets,conditions,exclude)
+        # else:
+        #     _sampler = self
+        worker = Worker(self,targets,conditions,exclude)
+        store = self.store[zarr_filename]
         
+        if parallel:
+            self.printlog("Parallel sampling started.")
+            n_pool = min( N // batch_size, cpu_count() ) 
+            self.printlog("Parallel sampling with",n_pool,"processes.")
+            with Pool(n_pool) as pool:
+                # NOTE: self.store.simulate(sampler, max_sims=None, batch_size=10)
+                pool.starmap(store.simulate, itertools.repeat([worker,None,batch_size],n_pool))
+        else:
+            store.simulate(worker, batch_size)
+
+
+class Worker:
+    """ Wrappter class passed to swyft.ZarrStore.simulate
+    """
+
+    def __init__(self,sampler,targets,conditions,exclude):
+        self.sampler = sampler
+        self.targets = targets
+        self.conditions = conditions
+        self.exclude = exclude
+
+    def __call__(self, size):
+        return self.sampler.sample(size,self.targets,self.conditions,self.exclude)
+
+
+class Network(swyft.SwyftModule):
+    def __init__(self,model,enable_1dim=True,enable_ndim=False,enable_embedding=False,dropout=0.1):
+        """ Define the network architecture 
+
+        NOTE: When using LogRatioEstimator_Ndim, it prepares internal networks for each marginal.
+        If you use embedding, the emmbedding layer is shared among all the marginals.
+        For example, if there is a very nice embeding for one embedding, it will be shared for the other marginals.
+        But is also means that the other marginals will potentially jam the training of this nice embedding,
+        so it could be helpful/harmful depending on the situation.
+
+        This is also the case when using ..._1dim and ..._Ndim simultaniously.
+        During the training step, the trainer will combine the two loss functions,
+        so the combining could be helpful/harmful depending on the situation as well.
+        """
+
+        # One (or both) of enable_1dim and enable_ndim must be True.
+        assert enable_1dim or enable_ndim
+
+        super().__init__()
+        self.model = model
+        self.ndim = model.ndim
+        self.enable_1dim = enable_1dim
+        self.enable_ndim = enable_ndim
+        self.enable_embedding = enable_embedding    
+        self.dropout = dropout
+
+        # define embedding
+        if enable_embedding:
+            n_embedding = model.ndim
+            self.embedding = torch.nn.Linear(len(model.data), n_embedding)
+        
+        num_features = self.n_embedding if enable_embedding else len(model.data)
+        self.logratios1 = swyft.LogRatioEstimator_1dim(
+            num_features = num_features, 
+            num_params = model.ndim, 
+            varnames = 'p',
+            dropout=dropout)
+        
+        # define marginals
+        if enable_ndim:
+            # self.marginals = [ (i,j) for i in range(model.ndim) for j in range(model.ndim) if i < j ]
+            self.marginals = [(0,1)]
+            self.logratios2 = swyft.LogRatioEstimator_Ndim(
+                num_features = num_features, 
+                marginals = self.marginals, 
+                varnames = 'p',
+                dropout=dropout)
+
+
+    def forward(self, A, B):
+        """ Define the forward pass of the network.
+        """
+        A = self.embedding(A['vlos_kms']) if self.enable_embedding else A['vlos_kms']
+
+        logratios = []
+        if self.enable_1dim:
+            logratios.append(self.logratios1(A, B['p']))
+        if self.enable_ndim:
+            logratios.append(self.logratios2(A, B['p']))
+
+        return logratios        
