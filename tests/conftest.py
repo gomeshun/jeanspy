f3c6df52e271697cd09cca73dcd7107888269512
Shunichi Horigome <37170876+gomeshun@users.noreply.github.com>
2026-08-27 00:08:16 +0900

    Remove obsolete legacy test collection exclusions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,4 @@
-import os
-from pathlib import Path
-
 import pytest
-
-
-_LEGACY_SWYFT_TEST_FILES = {"test_model.py", "swyfttest.py"}
-
-
-def pytest_ignore_collect(collection_path, config):
-    if os.environ.get("JEANSPY_ENABLE_LEGACY_SWYFT_TESTS") == "1":
-        return False
-    return Path(collection_path).name in _LEGACY_SWYFT_TEST_FILES
 
 
 def pytest_addoption(parser):
